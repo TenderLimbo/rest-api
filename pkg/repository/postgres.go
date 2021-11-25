9c15381e5b9efdad7f492f35e9469acc105f49af
TenderLimbo <nikitos-pashko2@mail.ru>
2021-11-25 17:03:37 +0300

    small refactoring

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -2,16 +2,9 @@ package repository
 
 import (
 	"fmt"
-	restapi "github.com/TenderLimbo/rest-api"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
-
-var Genres = []restapi.Genre{
-	{Name: "Adventure"},
-	{Name: "Classics"},
-	{Name: "Fantasy"},
-}
 
 func NewPostgresDB(config map[string]string, password string) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
